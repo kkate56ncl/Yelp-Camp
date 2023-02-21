d7613ac445240d4ee21acacaab6d644153f85302
Kate Chang <kate56ncl@gmail.com>
2023-02-21 19:42:06 +1100

    Another change in seeds index.js

--- seeds/index.js
+++ seeds/index.js
@@ -4,6 +4,10 @@ const Review = require("../models/review");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelps");
 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
 main().catch((err) => {
